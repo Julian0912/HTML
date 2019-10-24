81c5ceb2cf3b989321d177abb7f927e55769f2f2
Julian <1464861542@qq.com>
2019-10-24 12:08:52 +0800

    modify js code

--- First Homepage/conf/js/login.js
+++ First Homepage/conf/js/login.js
@@ -1,32 +1,35 @@
-function showModal(){
-	var fra = document.getElementById("modal");
-	var modal_list = fra.children;
-	for(var i=0;i<modal_list.length;i++){
-		modal_list[i].classList.remove("hide");
-	}
+function showModal() {
+    let fra = document.getElementById("modal");
+    let modal_list = fra.children;
+    for (let i = 0; i < modal_list.length; i++) {
+        modal_list[i].classList.remove("hide");
+    }
 }
-function hideModal(){
-	var fra = document.getElementById("modal");
-	var modal_list = fra.children;
-	for(var i=0;i<modal_list.length;i++){
-		modal_list[i].classList.add("hide");
-	}
+
+function hideModal() {
+    let fra = document.getElementById("modal");
+    let modal_list = fra.children;
+    for (let i = 0; i < modal_list.length; i++) {
+        modal_list[i].classList.add("hide");
+    }
 }
-function resetAll(){
-	var tags = document.getElementsByClassName("inputbox");
-	for(var i=0;i<tags.length;i++){
-		tags[i].value = "";
-	}
+
+function resetAll() {
+    let tags = document.getElementsByClassName("inputbox");
+    for (let i = 0; i < tags.length; i++) {
+        tags[i].value = "";
+    }
 }
-function log_in(){
-	var usr = "julian";
-	var pword = "123";
-	var username = document.getElementById("user").value;
-	var passwd = document.getElementById("pwd").value;
-	if(username==usr && passwd==pword){
-		window.open("bin/backpage.html")
-	}else{
-		document.getElementById("pwd").value = "";
-		alert("用户名或密码错误！");
-	}
+
+function log_in() {
+    let usr = "julian";
+    let pword = "123";
+    let username = document.getElementById("user").value;
+    let passwd = document.getElementById("pwd").value;
+    if (username === usr && passwd === pword) {
+        window.open("bin/backpage.html")
+    } else {
+        document.getElementById("pwd").value = "";
+        alert("用户名或密码错误！");
+    }
 }
